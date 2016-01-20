09be5c414f9ae5b7ed95131bdd5c34d03ad7cee8
norage <asdaonline@gmail.com>
2016-01-20 21:35:56 +0100

    Added some classes

---
+++ Backgammon/Backgammon/Backgammon Rules n Logic/PieceManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgammon.Backgammon_Rules_n_Logic
+{
+    public class PieceManager
+    {
+        //There are 30 pieces in total
+        private const Piece[] m_pieces = new Piece[30];
+
+        public PieceManager()
+        {
+
+        }
+        
+    }
+}
---
+++ Backgammon/Backgammon/Backgammon Rules n Logic/PiecePlacement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgammon.Backgammon_Rules_n_Logic
+{
+    public class PiecePlacement
+    {
+        //A two-dimentional array storing arrays
+        private const Piece[,][] m_Placement = new Piece[2, 12][];
+        private PieceManager m_pManager;
+
+        public PiecePlacement()
+        {
+            
+        }
+    }
+}
