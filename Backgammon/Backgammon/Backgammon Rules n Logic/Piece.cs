9be52f54b2e39b0562a3e32e1b82815a7eba26ad
norage <asdaonline@gmail.com>
2016-01-25 08:25:16 +0100

    Somewhat spaghetti

--- Backgammon/Backgammon/Backgammon Rules n Logic/Piece.cs
+++ Backgammon/Backgammon/Backgammon Rules n Logic/Piece.cs
@@ -9,24 +9,11 @@ namespace Backgammon.Backgammon_Rules_n_Logic
     public class Piece
     {
         private string m_pieceColor;
-        private int m_position;
-
-        public Piece()
-        {
-            m_pieceColor = string.Empty;
-            m_position = 0;
-        }
 
         public string PieceColor
         {
             get { return this.m_pieceColor; }
             set { this.m_pieceColor = value; }
-        }
-
-        public int Position
-        {
-            get { return this.m_position; }
-            set { this.m_position = value; }
         }
     }
 }
--- Backgammon/Backgammon/Backgammon Rules n Logic/PieceManager.cs
+++ Backgammon/Backgammon/Backgammon Rules n Logic/PieceManager.cs
@@ -9,12 +9,45 @@ namespace Backgammon.Backgammon_Rules_n_Logic
     public class PieceManager
     {
         //There are 30 pieces in total
-        private Piece[] m_pieces = new Piece[30];
+        private Piece[] m_WhitePieces;
+        private Piece[] m_RedPieces;
 
         public PieceManager()
         {
-
+            m_WhitePieces = new Piece[15];
+            m_RedPieces = new Piece[15];
+            CreatePieces();
         }
+
+        private void CreatePieces()
+        {
+            for (int i = 0; i < m_RedPieces.Length; i++)
+            {
+                Piece p = new Piece();
+                p.PieceColor = "White";
+                m_RedPieces[i] = p;
+            }
+
+            for (int i = 0; i < m_WhitePieces.Length; i++)
+            {
+                Piece p = new Piece();
+                p.PieceColor = "Red";
+                m_WhitePieces[i] = p;
+            }
+        }
+
+        public Piece WhitePiece(int index)
+        {
+            return m_WhitePieces[index];
+        }
+
+        public Piece RedPiece(int index)
+        {
+            return m_RedPieces[index];
+        }
+
+        
+        
         
     }
 }
